a2e1e5373eb2212263122a43888dbe5fe214fb66
Wen Jen <wjen1214@gmail.com>
2020-09-30 14:57:45 -0700

    initial commit

---
+++ models/author.js
@@ -0,0 +1,74 @@
+var mongoose = require('mongoose');
+var moment = require('moment');
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema(
+    {
+        first_name: {type: String, required: true, maxlength: 100},
+        family_name: {type: String, required: true, maxlength: 100},
+        date_of_birth: {type: Date},
+        date_of_death: {type: Date},
+    }
+);
+
+AuthorSchema
+.virtual('date_of_birth_formatted')
+.get(function() {
+    return this.date_of_birth ? moment(this.date_of_birth).format('YYYY-MM-DD') : '';
+})
+
+
+AuthorSchema
+.virtual('date_of_death_formatted')
+.get(function() {
+    return this.date_of_death ? moment(this.date_of_death).format('YYYY-MM-DD') : '';
+})
+
+AuthorSchema.virtual('date_of_birth_yyyy_mm_dd').get(function() {
+    return moment(this.date_of_birth).format('YYYY-MM-DD');
+  });
+  
+  AuthorSchema.virtual('date_of_death_yyyy_mm_dd').get(function() {
+    return moment(this.date_of_death).format('YYYY-MM-DD');
+  });
+// Virtual for author's full name
+AuthorSchema
+.virtual('name')
+.get(function () {
+
+// To avoid errors in cases where an author does not have either a family name or first name
+// We want to make sure we handle the exception by returning an empty string for that case
+
+    var fullname = '';
+    if (this.first_name && this.family_name) {
+        fullname = this.family_name + ', ' + this.first_name
+    }
+    if (!this.first_name || !this.family_name) {
+        fullname = '';
+    }
+
+    return fullname;
+});
+
+AuthorSchema.virtual('lifespan').get(function() {
+    var lifetime_string = '';
+    if (this.date_of_birth) {
+      lifetime_string = moment(this.date_of_birth).format('MMMM Do, YYYY');
+    }
+    lifetime_string += ' - ';
+    if (this.date_of_death) {
+      lifetime_string += moment(this.date_of_death).format('MMMM Do, YYYY');
+    }
+    return lifetime_string;
+  });
+
+// Virtual for author's URL
+AuthorSchema
+.virtual('url')
+.get(function () {
+    return '/catalog/author/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Author', AuthorSchema);
